1e30c5b07c4ffa2a01dd39daf45d1bb2554f9a2c
Franklin Cappa Ticona <franklincappa@gmail.com>
2025-04-23 07:33:09 -0500

    Update karma sandbox Chrome

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
       }
     },
     singleRun: true,
